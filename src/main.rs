b46303a0b602ec14eeb61e8db2aea61a877cb758
Alejandro González Momblán <agelrenorenardo@gmail.com>
2023-10-07 14:55:25 +0200

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn handle_client(mut stream: TcpStream) {
+    // Define the HTTP response
+    let response = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n";
+    
+    // Write the response to the TCP stream
+    if let Err(err) = stream.write_all(response.as_bytes()) {
+        eprintln!("Error writing to stream: {}", err);
+    } else {
+        println!("Sent 200 OK response");
+    }
+
+    // Read and ignore data from the connection (you can process it later)
+    let mut buffer = [0; 1024];
+    while let Ok(n) = stream.read(&mut buffer) {
+        if n == 0 {
+            break;
+        }
+    }
+
+    println!("Finished processing connection");
+}
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -8,27 +31,16 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut tcp_stream) => {
-                // Define the HTTP response
-                let response = "HTTP/1.1 200 OK\r\n\r\n";
+            Ok(tcp_stream) => {
+                println!("Accepted new connection");
 
-                // Write the response to the TCP stream
-                if let Err(err) = tcp_stream.write_all(response.as_bytes()) {
-                    eprintln!("Error writing to stream: {}", err);
-                }
-
-                // Read and ignore data from the connection (you can process it later)
-                let mut buffer = [0; 1024];
-                while let Ok(n) = tcp_stream.read(&mut buffer) {
-                    if n == 0 {
-                        break;
-                    }
-                }
-
-                println!("accepted new connection and sent 200 OK response");
+                // Spawn a new thread to handle the client
+                thread::spawn(move || {
+                    handle_client(tcp_stream);
+                });
             }
             Err(e) => {
-                println!("error: {}", e);
+                eprintln!("Error accepting connection: {}", e);
             }
         }
     }
